d9db17cf91740257ea053d5607e1840b5896d977
ali <aliriza_altiparmak@hotmail.com>
2025-03-31 20:00:00 +0200

    without cloudfront

--- terraform/website.tf
+++ terraform/website.tf
@@ -1,5 +1,5 @@
 module "website" {
-  source          = "github.com/araltiparmak/terraform-modules//static-site?ref=v1.0.1"
+  source          = "github.com/araltiparmak/terraform-modules//static-site?ref=v1.1.0"
   domain_name     = local.bucket
-  with_cloudfront = true
+  with_cloudfront = false
 }
